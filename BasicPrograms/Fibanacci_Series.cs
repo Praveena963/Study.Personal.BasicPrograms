828271d0bb6d9df6866c13434d110a814d14d2af
vadde <vadde@DESKTOP-DFCEJMB>
2023-04-17 08:42:01 +0530

    Add project files.

---
+++ BasicPrograms/Fibanacci_Series.cs
@@ -0,0 +1,23 @@
+﻿namespace BasicPrograms
+{
+    public class Fibanacci_Series
+    {
+        //static int a = 0;static int b = 1; static int c;
+        static void Main()
+        {
+            int a = 0;  int b = 1; 
+            int c;
+            Console.WriteLine("Series is");
+            Console.Write(a + "" + b);
+
+
+            for (int i = 2; i < 10; i++)
+            {
+                c = a + b;
+                Console.Write(c);
+                a = b; b = c;
+            }
+            Console.WriteLine();
+        }
+    }
+}
